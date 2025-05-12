72742ebbce6820e222ee9974dab3f8cc668b2d28
Anastasia <nastiavlasova28@gmail.com>
2025-05-12 20:01:35 +0300

    add addition operation

--- function.py
+++ function.py
@@ -1,2 +1,5 @@
 def calculate_sum(numbers):
     return sum(numbers)
+
+def calculate_difference(a, b):
+    return a - b
--- test_function.py
+++ test_function.py
@@ -1,6 +1,7 @@
 import unittest
 from function import (
-    calculate_sum
+    calculate_sum,
+    calculate_difference
 )
 
 class TestMathOperations(unittest.TestCase):
@@ -9,6 +10,9 @@ class TestMathOperations(unittest.TestCase):
         self.assertEqual(calculate_sum([1, 2, 3]), 6)
         self.assertEqual(calculate_sum([]), 0)
         self.assertEqual(calculate_sum([-1, 1]), 0)
+
+    def test_difference(self):
+        self.assertEqual(calculate_difference(10, 5), 5)
 
 if __name__ == '__main__':
     unittest.main()
