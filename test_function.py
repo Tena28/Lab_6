c1d59d612432923bf7a49a26162e5d123ac83fe3
Anastasia <nastiavlasova28@gmail.com>
2025-05-17 20:26:56 +0300

    release v1.0 your surname

---
+++ test_function.py
@@ -0,0 +1,30 @@
+import unittest
+from function import (
+    calculate_sum,
+    calculate_difference,
+    calculate_product,
+    calculate_division
+)
+
+class TestMathOperations(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(calculate_sum([1, 2, 3]), 6)
+        self.assertEqual(calculate_sum([]), 0)
+        self.assertEqual(calculate_sum([-1, 1]), 0)
+
+    def test_difference(self):
+        self.assertEqual(calculate_difference(10, 5), 5)
+
+    def test_product(self):
+        self.assertEqual(calculate_product([1, 2, 3]), 6)
+
+    def test_division(self):
+        self.assertEqual(calculate_division(10, 2), 5)
+
+    def test_division_by_zero(self):
+        with self.assertRaises(ValueError):
+            calculate_division(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
